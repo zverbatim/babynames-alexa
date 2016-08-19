159da6bfe6a39d323f9b7d5f0f10c083cc394642
zverbatim <imunteanu@unomaha.edu>
2016-08-18 22:47:08 -0500

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+echo "START"
+
+START_TIME=$SECONDS
+
+echo "1. build it ..."
+mvn assembly:assembly -DdescriptorId=jar-with-dependencies package
+
+echo "2. mv .jar .zip"
+mv target/babynames-alexa-1.0-jar-with-dependencies.jar target/babynames-alexa-1.0-jar-with-dependencies.zip
+
+echo "3. invoke the aws cli to deploy ..."
+aws lambda update-function-code --function-name BabyNames --zip-file fileb://./target/babynames-alexa-1.0-jar-with-dependencies.zip
+
+echo "DONE. It took $(($SECONDS - $START_TIME)) seconds."
